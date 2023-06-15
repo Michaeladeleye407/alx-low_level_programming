90944d157c37030f76a1428c99751b714a752f26
Michaeladeleye407 <Michaeladeleye407@gmail.com>
2023-06-15 14:47:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
-include <stdio.h>
-
-int main() {
-	put("Programming is like building a multilingual puzzle");
-	return 0;
+#include <stdio.h>
+/**
+ * main - A programming language function thats outstanding and perfect
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+Puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
